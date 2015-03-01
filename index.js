4449ef7926cc9d44c9eca21791b11209f3d2000e
Denis <denis.pav@hotmail.com>
2015-03-01 15:21:42 +0100

    Added new model used for tracking of active groups via sockets, also implemented counting of users inside of each active group, delay is set to 1s per check

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 var app = require('_/app'),
+    mongoose = require('mongoose'),
+    models = require('./lib/app/models/index.js'),
+    async = require('async'),
     config = require('_/config'),
     socketController = require('_/app/'),
     io = require('socket.io'),
     server = require('http').createServer(app).listen(config.port),
     sio = io.listen(server);
+
+var interval = setInterval(function(){
+
+    models.active.find({ active: true }, function(err, activeGroups){
+        console.log(activeGroups);
+
+        for(var i = 0; i<activeGroups.length; i++){
+            //console.log(activeGroups[i].id);
+            //sio.sockets.in(activeGroups[i].id).emit('active', { id: activeGroups[i].id });
+            
+            var res = []
+            , room = sio.sockets.adapter.rooms[activeGroups[i].id];
+            if (room) {
+                for (var id in room) {
+                res.push(sio.sockets.adapter.nsp.connected[id]);
+                }
+            }
+
+            if(room)
+                console.log('People: ' + JSON.stringify(room));
+            else
+                console.log('No people!');
+        }
+    });
+
+}, 1000);
+
 sio.on('connection', function(socket) {
     console.log("Client connected");
     // sockets.initialize(socket,sio);
@@ -13,6 +43,28 @@ sio.on('connection', function(socket) {
         socket.group = newGroupId;
         socket.username = username;
         socket.join(newGroupId);
+
+        async.waterfall([
+            function(callback){
+                models.active.findOne({id: newGroupId, active: false}, function(err, active){
+                    if(err){
+                        callback(err);
+                    } else if (active){
+                        callback(null, active);
+                    }
+                });
+            },
+            function(activeGroup, callback){
+                models.active.findByIdAndUpdate(activeGroup._id, { active: true }, function(err, updated){
+                    if(err){
+                        callback(err);
+                    }
+                });
+            }
+        ], function(error){
+            console.log('Error seting activeGroup: ' + error);
+        });
+
     });
     socket.on('songAdded', function(track) {
         console.log('Track');
